123e806654074a0591290db4e879f7a89502be0d
Franco4848 <francoalvarez4848mail.com>
2024-05-12 20:14:25 -0300

    se arreglo la bd

--- servidor3.py
+++ servidor3.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, Response, session
 from flask_mysqldb import MySQL , MySQLdb
-
+from dotenv import load_dotenv
+import os
 app = Flask(__name__,static_url_path= '/static')
-app.config['MYSQL_DB'] = 'super'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['SECRET_KEY'] = '4848'
+load_dotenv()
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_CURSORCLASS'] = os.getenv("MYSQL_CURSORCLASS")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
 
 mysql = MySQL(app)
 
